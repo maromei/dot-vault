ac99ba1b62ad01f189650111441ffdc924d0df4b
maromei <maromei@proton.me>
2026-06-04 23:18:06 +0200

    feat: module list command

--- src/dot_vault/__main__.py
+++ src/dot_vault/__main__.py
@@ -1,5 +1,11 @@
-import doctyper as typer
+from pathlib import Path
 
+import doctyper as typer
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+
+from dot_vault.paths import get_module_dir
 from dot_vault.modules import Module, get_module
 
 
@@ -21,6 +27,33 @@ def modules_install(module: str, script: str | None = None):
 
     module_obj: Module = get_module(name=module)
     module_obj.install(install_script=script)
+
+
+@module_app.command(name="list")
+def modules_list():
+    """List all available modules."""
+
+    module_dir: Path = get_module_dir()
+    module_names: list[str] = [d.name for d in module_dir.iterdir() if d.is_dir()]
+    modules: list[Module] = [get_module(name) for name in module_names]
+
+    table = Table(show_header=False, box=None, padding=(0, 2, 0, 0))
+
+    table.add_column(style="bold cyan")
+    table.add_column()
+
+    for module in modules:
+        table.add_row(module.name, module.config.description)
+
+    title = "Available Modules"
+    subtitle: str = module_dir.as_posix()
+
+    panel = Panel(
+        table, title=title, title_align="left", border_style="dim", subtitle=subtitle
+    )
+
+    console = Console()
+    console.print(panel)
 
 
 if __name__ == "__main__":
