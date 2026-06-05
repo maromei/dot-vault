076163573a7ae48a800e4c66e63d351e4dcb44a0
maromei <maromei@proton.me>
2026-06-05 21:25:25 +0200

    feat: add function to get path to check_installed script

--- src/dot_vault/paths.py
+++ src/dot_vault/paths.py
@@ -9,6 +9,8 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from dot_vault.errors import MoreThanOneFileFound
+
 
 def get_dot_vault_dir() -> Path:
     """Get the directory containing the dot-vault config.
@@ -58,3 +60,42 @@ def get_module_dir(name: str | None = None) -> Path:
     if not module_dir.is_dir():
         raise FileNotFoundError("Module directory does not exist.")
     return module_dir
+
+
+def get_check_installed_script(module_name: str, target: str | None) -> Path | None:
+    """Get the path to the script to check if a module is installed.
+
+    Searches the following of :func:`get_module_dir``{module_name}/check_installed/`
+    for the `{target}*` file. If `target` is `None`, it is assumed to
+    only contain one file whose path will be returned. An error will be raised if
+    multiple files are found for the given pattern.
+
+    Raises:
+        MoreThanOneFileFound: More than one relevant file found.
+
+    Args:
+        module_name:
+        target: target to check for. If `None`, assumes only a single script exists.
+
+    Returns:
+        If the module is installed, or `None` if no `check_installed` script
+        exists.
+    """
+
+    module_dir: Path = get_module_dir(module_name)
+    script_dir: Path = module_dir / "check_installed"
+    if not script_dir.is_dir():
+        return None
+
+    pattern: str = "*"
+    if target is not None:
+        pattern = f"{target}*"
+
+    matching_files: list[Path] = list(script_dir.glob(pattern))
+    if len(matching_files) == 0:
+        return None
+
+    if len(matching_files) > 1:
+        raise MoreThanOneFileFound("Found more than 1 check_installed script.")
+
+    return matching_files[0]
