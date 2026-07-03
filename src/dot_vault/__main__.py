1912ccebe08eae31cebdc0f60c604899af5dc3c9
maromei <maromei@proton.me>
2026-07-03 12:56:26 +0200

    refactor: module list command uses pretty_print module

--- src/dot_vault/__main__.py
+++ src/dot_vault/__main__.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 
 import doctyper as typer
-from rich.console import Console
-from rich.table import Table
-from rich.panel import Panel
 
 from dot_vault.paths import get_module_dir
 from dot_vault.modules import Module, get_module, ReturnFile
@@ -58,7 +55,7 @@ def modules_check_installed(module: str, target: str | None = None):
     table_data: list[tuple[str, str]] = [(k, str(v)) for k, v in toml_content]  # pyright: ignore[reportAny]
 
     title: str = f"Is {module} installed?"
-    print_two_column_table(table_data = table_data, title = title)
+    print_two_column_table(table_data=table_data, title=title)
 
 
 @module_app.command(name="list")
@@ -69,23 +66,14 @@ def modules_list():
     module_names: list[str] = [d.name for d in module_dir.iterdir() if d.is_dir()]
     modules: list[Module] = [get_module(name) for name in module_names]
 
-    table = Table(show_header=False, box=None, padding=(0, 2, 0, 0))
-
-    table.add_column(style="bold cyan")
-    table.add_column()
-
-    for module in modules:
-        table.add_row(module.name, module.config.description)
+    table_data: list[tuple[str, str]] = [
+        (module.name, module.config.description) for module in modules
+    ]
 
     title = "Available Modules"
     subtitle: str = module_dir.as_posix()
 
-    panel = Panel(
-        table, title=title, title_align="left", border_style="dim", subtitle=subtitle
-    )
-
-    console = Console()
-    console.print(panel)
+    print_two_column_table(table_data=table_data, title=title, subtitle=subtitle)
 
 
 if __name__ == "__main__":
